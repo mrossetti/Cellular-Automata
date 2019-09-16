7f0ab4ae0b679037efffdfc6354e42011978c1bd
Marco Rossetti <rossetti.marcolm@gmail.com>
2019-09-15 20:54:47 -0500

    Update Cellular Automata.py
    Improved description

--- Cellular Automata.py
+++ Cellular Automata.py
@@ -12,8 +12,8 @@ def create_map(rows, cols):
         - spawn cells where many cells alive (density -> more density)
 
     Implementation:
-        - Measure the local (for each cell) sparsity/density by counting
-          the cell's alive neighbors (other cells in a radius of +-1)
+        - For each cell, measure the local sparsity/density by counting
+          the cell's alive neighbors (other cells in unit radius)
         - Change the current cell's state when this count exceeds some
           threshold values (e.g. spawn a cell if count > birth_limit).
     """
@@ -25,17 +25,10 @@ def create_map(rows, cols):
     birth_limit = 4    # set alive if n. alive neighbors > birth_limit
 
     # map initialization
-    # We create an expanded array of size (rows+2, cols+2) which will act as
-    # outer frame (made of ones) for the true inner map of size (rows, cols).
-    # This comes handy for counting the neighbors of edge cells which, in this
-    # way, will always be considered alive (1)
-    map = np.ones((rows+2, cols+2))
-    # The inner map we actually care to return and update has size (rows, cols)
-    # and is randomly initialized with zeros and ones
+    map = np.ones((rows+2, cols+2))  # out-of-bounds considered alive (1)
     inner = np.random.choice(a=[1, 0],
                              size=(rows, cols),
                              p=[init_chance, 1-init_chance])
-    # The inner map starts at 1 and ends at axis+1 (axis+2-1 = axis+1)
     map[1:rows+1, 1:cols+1] = inner
 
     # map creation
@@ -60,27 +53,16 @@ def create_map(rows, cols):
                 else:
                     if n > birth_limit:
                         map[row][col] = 1
-
-    # return inner
+    
     return map[1:rows+1, 1:cols+1]
-
-def encode_map(map, symbols):
-    symbol_true, symbol_false = symbols
-    return np.where(map, symbol_true, symbol_false)
-
-def print_arr(arr):
-    for i in range(arr.shape[0]):
-        for j in range(arr.shape[1]):
-            print(arr[i][j], end='')
-        print()
 
 
 def main():
     # example
     np.random.seed(1)
     map = create_map(20, 40)
-    map = encode_map(map, ['#', ' '])
-    print_arr(map)
+    map = np.where(map, '#', ' ')
+    print(map)
 
 
 if __name__ == '__main__':
